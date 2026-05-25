36d9707ff2999cb696dac45090d1e1a2861d6df9
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-05-26 01:18:05 +0600

    iFrame Testing

--- tests/example5.spec.js
+++ tests/example5.spec.js
@@ -16,11 +16,41 @@ test.describe("Different Tests", () => {
         await page.dragAndDrop('#column-a', '#column-b');
         await page.dragAndDrop('#column-b', '#column-a');
     })
-    test("Testing Drop Down", async ({ page }) => {
+    test.skip("Testing Drop Down", async ({ page }) => {
         await page.goto('/dropdown');
         await page.locator('#dropdown').selectOption('1');
         await expect(page.locator('#dropdown')).toHaveValue('1');
         await page.locator('#dropdown').selectOption({ label: 'Option 2' });
         await expect(page.locator('#dropdown')).toHaveValue('2');
+    })
+    test("Testing iFrames", async ({ page }) => {
+        await page.goto('https://vinothqaacademy.com/iframe/');
+        const frameTest = page.frameLocator('iframe[name=registeruser]');
+        await frameTest.locator('#vfb-5').fill('Capt.');
+        await frameTest.locator('#vfb-7').fill('MacTavish');
+        await frameTest.locator('#vfb-31-1').check();
+        await frameTest.locator('#vfb-20-0').check();
+        await frameTest.locator('#vfb-20-1').check();
+        await frameTest.locator('#vfb-20-3').uncheck();
+        await frameTest.locator('#vfb-20-4').check();
+        await frameTest.locator('#vfb-13-address').fill('69');
+        await frameTest.locator('#vfb-13-address-2').fill('1');
+        await frameTest.locator('#vfb-13-city').fill('Herefordshire');
+        await frameTest.locator('#vfb-13-state').fill('Herefordshire');
+        await frameTest.locator('#vfb-13-zip').fill('141');
+        await frameTest.locator('[aria-controls="select2-vfb-13-country-container"]').click();
+        await frameTest.locator('.select2-search__field').fill('Bangladesh');
+        await frameTest.locator('li.select2-results__option', { hasText: 'Bangladesh' }).first().click();
+        await frameTest.locator('#vfb-14').fill('capt.mactavish@sas.uk');
+        await frameTest.locator('#vfb-18').fill('05/26/26');
+        await frameTest.locator('[aria-labelledby="select2-vfb-16-hour-container"]').click();
+        await frameTest.locator('li.select2-results__option', { hasText: '05' }).click();
+        await frameTest.locator('#vfb-19').fill('01585897854');
+        await frameTest.locator('#vfb-23').fill('Test');
+        const exampleLabel = await frameTest.locator('text=/Example:\\s*\\d+/').textContent();
+        const match = exampleLabel.match(/\d+/);
+        const value = match ? match[0] : '33';
+        await frameTest.locator('#vfb-3').fill(value);
+        await frameTest.locator('#vfb-4').click();
     })
 })
