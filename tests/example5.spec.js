f3fd391f24adb59d4d832a991c7d5cda0d9feed6
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-05-25 22:10:06 +0600

    Drag And Drop

--- tests/example5.spec.js
+++ tests/example5.spec.js
@@ -1,14 +1,19 @@
 const { test, expect } = require('@playwright/test');
 
-test.describe("Checkbox", () => {
+test.describe("Different Tests", () => {
 
     test.beforeEach(async ({ page }) => {
         await page.goto('https://the-internet.herokuapp.com/');
     })
 
-    test("Testing Checkbox", async ({ page }) => {
+    test.skip("Testing Checkbox", async ({ page }) => {
         await page.goto('/checkboxes');
         await page.locator("input[type='checkbox']").first().check();
         await page.locator("input[type='checkbox']").last().uncheck();
+    })
+    test("Testing Drag & Drop", async ({ page }) => {
+        await page.goto('/drag_and_drop');
+        await page.dragAndDrop('#column-a', '#column-b');
+        await page.dragAndDrop('#column-b', '#column-a');
     })
 })
